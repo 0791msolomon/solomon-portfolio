4b438674ca237579566a43984eb58fcb1c3624ab
Matt <0791msolomon@gmail.com>
2018-10-17 12:01:08 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
+// const router = require("./app/routes/index");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.Promise = global.Promise;
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+// const generatePassword = require("password-generator");
+
+// app.use(router);
+
+mongoose.connect(
+  MONGODB_URI || "mongodb://localhost:27017/portfolio",
+  { useNewUrlParser: true }
+);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
